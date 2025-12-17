7e13a6c35d192c3a450da214a09af6d4f6619f09
bpshift001@bancoctt.pt <bpshift001@bancoctt.pt>
2025-12-17 18:49:06 +0000

    Add new .NET 5.0 projects and implement a minimal API with basic operations

---
+++ src/Aula 03 - API Minimalista com .NET 10/01-CodeMigration/ExemploTDD/Projeto.LogicaNegocio/Validador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto.LogicaNegocio
+{
+    public class Validador
+    {
+        public bool ValidarNif(string param)
+        {
+            if (param.Length != 9 || param[0] == '1')
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/Aula 03 - API Minimalista com .NET 10/01-CodeMigration/ExemploTDD/Projeto.Tests/ValidadorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto.LogicaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.LogicaNegocio.Tests
+{
+    [TestClass()]
+    public class ValidadorTests
+    {
+        [TestMethod()]
+        public void DeveValidarSeTem9Caracteres()
+        {
+            //Arrange - Preparacao
+            var nifExemplo = "223456789";
+
+            //Act - Ação 
+            var validador = new Validador();
+            var resultadoObtido = validador.ValidarNif(nifExemplo);
+
+            //Assert - Validacao
+            Assert.IsTrue(resultadoObtido);
+        }
+
+
+      
+
+        [TestMethod]
+        public void NaoDeveAceitarNifComecandoCom1()
+        {
+            //Arrange
+            var nifExemplo = "123456789";
+
+            //Act
+            var validador = new Validador();
+            var resultadoObtido = validador.ValidarNif(nifExemplo);
+
+            //Assert
+            Assert.IsFalse(resultadoObtido);
+        }
+    }
+}
